674023a0f31b86590f8864bffed6a910b6fdf0f2
Richard Martens <richard.martens@bitech.ag>
2016-07-22 08:09:57 +0200

    support ui5 components

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(require('stylus').middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'components')));
 
 app.use('/', routes);
 app.use('/users', users);
